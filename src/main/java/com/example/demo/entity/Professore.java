0c68cec32f831aaa504ce3be2cfdd5a8f6447e46
CSGIGIK <giginter99@gmail.com>
2026-02-26 20:37:56 +0100

    feat: imported Lombok dependency, modified Professore and Studente - no boilerplate code needed

--- src/main/java/com/example/demo/entity/Professore.java
+++ src/main/java/com/example/demo/entity/Professore.java
@@ -7,9 +7,15 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 
 @Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 @Table(name = "professore")
 public class Professore {
     
@@ -49,118 +55,6 @@ public class Professore {
     
     @Column(length = 100)           
     private String ufficio;          //(es: "Aula 205")
-
-    
-    public Professore() {}
-    
-    public Professore(Long id, String matricola, String nome, String cognome, String email, String username,
-            String password, String ruolo, LocalDateTime dataAssunzione, String ruoloAccademico, String telefono,
-            String ufficio) {
-        this.id = id;
-        this.matricola = matricola;
-        this.nome = nome;
-        this.cognome = cognome;
-        this.email = email;
-        this.username = username;
-        this.password = password;
-        this.ruolo = ruolo;
-        this.dataAssunzione = dataAssunzione;
-        this.ruoloAccademico = ruoloAccademico;
-        this.telefono = telefono;
-        this.ufficio = ufficio;
     }
 
-    
-    public Long getId() {
-        return id;
-    }
 
-    public String getMatricola() {
-        return matricola;
-    }
-
-    public void setMatricola(String matricola) {
-        this.matricola = matricola;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public String getCognome() {
-        return cognome;
-    }
-
-    public void setCognome(String cognome) {
-        this.cognome = cognome;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getRuolo() {
-        return ruolo;
-    }
-
-    public void setRuolo(String ruolo) {
-        this.ruolo = ruolo;
-    }
-
-    public LocalDateTime getDataAssunzione() {
-        return dataAssunzione;
-    }
-
-    public void setDataAssunzione(LocalDateTime dataAssunzione) {
-        this.dataAssunzione = dataAssunzione;
-    }
-
-    public String getRuoloAccademico() {
-        return ruoloAccademico;
-    }
-
-    public void setRuoloAccademico(String ruoloAccademico) {
-        this.ruoloAccademico = ruoloAccademico;
-    }
-
-    public String getTelefono() {
-        return telefono;
-    }
-
-    public void setTelefono(String telefono) {
-        this.telefono = telefono;
-    }
-
-    public String getUfficio() {
-        return ufficio;
-    }
-
-    public void setUfficio(String ufficio) {
-        this.ufficio = ufficio;
-    }
-
-}
--- src/main/java/com/example/demo/entity/Studente.java
+++ src/main/java/com/example/demo/entity/Studente.java
@@ -9,8 +9,15 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity // indichiamo che questa classe è un'entità JPA
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+
 @Table(name = "studente") // indichiamo il nome della tabella nel database a cui questa classe è associata
                           // se non ce la crea automaticamente
 
@@ -72,143 +79,4 @@ public class Studente {
     @Column(length = 20)
     private String telefono;
 // definiamo i costruttori, i getter e i setter per i campi della classe Studente
-
- public Studente() {}
-
-    public Studente(String matricola, String nome, String cognome, String email, String username, String password,
-            LocalDateTime dataIscrizione, String ruolo, boolean iscritto, int versione, String codiceFiscale,
-            java.time.LocalDate dataNascita, String luogoNascita, String telefono) {
-        this.matricola = matricola;
-        this.nome = nome;
-        this.cognome = cognome;
-        this.email = email;
-        this.username = username;
-        this.password = password;
-        this.dataIscrizione = dataIscrizione;
-        this.ruolo = ruolo;
-        this.iscritto = iscritto;
-        this.versione = versione;
-        this.codiceFiscale = codiceFiscale;
-        this.dataNascita = dataNascita;
-        this.luogoNascita = luogoNascita;
-        this.telefono = telefono;
-    }
-
-//==========GETTER==========//SETTER==========//
-  // ID
-public Long getId() {
-    return id;
-}
-
-// Matricola  
-public String getMatricola() {
-    return matricola;
-}
-public void setMatricola(String matricola) {
-    this.matricola = matricola;
-}
-
-// Nome
-public String getNome() {
-    return nome;
-}
-public void setNome(String nome) {
-    this.nome = nome;
-}
-
-// Cognome
-public String getCognome() {
-    return cognome;
-}
-public void setCognome(String cognome) {
-    this.cognome = cognome;
-}
-
-// Email
-public String getEmail() {
-    return email;
-}
-public void setEmail(String email) {
-    this.email = email;
-}
-
-// Username
-public String getUsername() {
-    return username;
-}
-public void setUsername(String username) {
-    this.username = username;
-}
-
-// Password
-public String getPassword() {
-    return password;
-}
-public void setPassword(String password) {
-    this.password = password;
-}
-
-// DataIscrizione
-public LocalDateTime getDataIscrizione() {
-    return dataIscrizione;
-}
-public void setDataIscrizione(LocalDateTime dataIscrizione) {
-    this.dataIscrizione = dataIscrizione;
-}
-
-// Ruolo
-public String getRuolo() {
-    return ruolo;
-}
-public void setRuolo(String ruolo) {
-    this.ruolo = ruolo;
-}
-
-// Iscritto
-public boolean isIscritto() {
-    return iscritto;
-}
-public void setIscritto(boolean iscritto) {
-    this.iscritto = iscritto;
-}
-
-// Versione
-public int getVersione() {
-    return versione;
-}
-public void setVersione(int versione) {
-    this.versione = versione;
-}
-
-// CodiceFiscale 
-public String getCodiceFiscale() {
-    return codiceFiscale;
-}
-public void setCodiceFiscale(String codiceFiscale) {
-    this.codiceFiscale = codiceFiscale;
-}
-
-// DataNascita 
-public java.time.LocalDate getDataNascita() {
-    return dataNascita;
-}
-public void setDataNascita(java.time.LocalDate dataNascita) {
-    this.dataNascita = dataNascita;
-}
-
-// LuogoNascita  
-public String getLuogoNascita() {
-    return luogoNascita;
-}
-public void setLuogoNascita(String luogoNascita) {
-    this.luogoNascita = luogoNascita;
-}
-
-// Telefono 
-public String getTelefono() {
-    return telefono;
-}
-public void setTelefono(String telefono) {
-    this.telefono = telefono;
-}
 }
